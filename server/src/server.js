ab732fcdd29d56a4e7f8349ef6edd3d22fe32144
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-10-27 10:29:35 +0530

    feat/created new view model without arwes

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,11 @@
+const express = require('express');
 // Since react runs on port 3000, server must be on another port
 // if PORT is not specified in the environment, 8000 will be used
 const PORT = process.env.PORT || 8000;
+
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+	console.log(`Listening on PORT ${PORT}`);
+})
---
+++ view/src/App.js
@@ -0,0 +1,15 @@
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
+
+import AppLayout from "./pages/AppLayout";
+
+// import { resources } from "./settings";
+
+const App = () => {
+	<Router>
+		<AppLayout />
+	</Router>
+};
+
+export default App;

284a856bedece959b809e460469363dbc0b8389f
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-10-29 16:12:24 +0530

    feat/added form inputs in launch.jsx

--- view/src/Routes/Launch.jsx
+++ view/src/Routes/Launch.jsx
@@ -1,4 +1,6 @@
 const Launch = () => {
+	const today = new Date().toISOString().split("T")[0];
+
 	return (
 		<div className="w-2/3 p-6 border-2 mt-6 mx-auto">
 			<p className="text-2xl">Schedule a mission launch for interstellar travel to one of the kepler Exoplanets.</p>
@@ -8,6 +10,39 @@ const Launch = () => {
 				<li>Planetary radius &#60; 1.6 times Earth's radius</li>
 				<li>Effective stellar flux &#60; 0.36 times Earth's value and &#60; 1.11 times Earth's value</li>
 			</ul>
+			
+			<form>
+				<label htmlFor="launch-date">Launch Date</label>
+				<input 
+					type="date" 
+					id="launch-day" 
+					name="launch-day" 
+					min={today} 
+					max="2030-12-31" 
+					defaultValue={today} 
+				/>
+				<label htmlFor="mission-name">Mission Name</label>
+				<input 
+					type="text"
+					id="mission-name"
+					name="mission-name"
+				/>
+				<label htmlFor="rocket-name">Mission Name</label>
+				<input 
+					type="text"
+					id="rocket-name"
+					name="rocket-name"
+					defaultValue="Explorer IS1"
+				/>
+				<label htmlFor="planets-selector">Destination Exoplanet</label>
+				<select id="planets-selector" name="planets-selector">
+					<option value=""></option>
+				</select>
+				<button
+					type="submit">
+					Launch Mission
+				</button>
+			</form>
 		</div>
 	)
 }

672b227137531e90a587e8d850bf802d7f715baf
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-10-30 13:29:11 +0530

    feat/added svg logo in the nabigation

--- view/src/Routes/Navigation.jsx
+++ view/src/Routes/Navigation.jsx
@@ -1,11 +1,15 @@
 import { Link, Outlet } from 'react-router-dom';
 import { Fragment } from 'react';
+import { ReactComponent as Logo } from '../img/solar2.svg';
 
 const Navigation = () => {
 	return (
 		<Fragment>
 			<nav className="flex py-4  border-transparent border-b-primary border-b-4 text-2xl space-x-10 justify-center">
-				<h3>Starship Launch</h3>
+				<h3>
+					<Logo className="w-8 inline-block mr-2"/> 
+					Starship Launch
+				</h3>
 				<Link className="text-lightPrimary" to="/">Launch</Link>	
 				<Link className="text-lightPrimary" to="/upcoming">Upcoming</Link>	
 				<Link className="text-lightPrimary" to="/history">History</Link>	

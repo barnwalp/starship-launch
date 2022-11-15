6d8e29b95aedcfcba452e1aa010735f074e334ba
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-11-15 10:32:55 +0530

    feat/styling launch table

--- view/src/Routes/Upcoming.jsx
+++ view/src/Routes/Upcoming.jsx
@@ -14,12 +14,12 @@ const Upcoming = () => {
 	console.log(launches);
 	const launchTable = launches.map((launch) => {
 		return (
-			<tr>
-				<td>{launch.flightNumber}</td>
-				<td>{launch.launchDate}</td>
-				<td>{launch.mission}</td>
-				<td>{launch.rocket}</td>
-				<td>{launch.destinanation}</td>
+			<tr className="">
+				<td className="font-light py-1 text-center">{launch.flightNumber}</td>
+				<td className="font-light text-center">{launch.launchDate}</td>
+				<td className="font-light text-center">{launch.mission}</td>
+				<td className="font-light text-center">{launch.rocket}</td>
+				<td className="font-light text-center">{launch.destination}</td>
 			</tr>
 		)
 	})
@@ -28,13 +28,13 @@ const Upcoming = () => {
 			<div className="w-2/3 p-6 border-2 mt-6 mx-auto">
 				<p className="text-2xl">Upcoming missions including both SpaceX launches and newly scheduled ISRO GSLV-III rockets.</p>
 				<p className="text-2xl mt-3">Warning! Clicking on the X aborts the mission.</p>
-				<table>
-					<tr>
-						<th>No</th>
-						<th>Date</th>
-						<th>Mission</th>
-						<th>Rocket</th>
-						<th>Destination</th>
+				<table className="mt-3 w-full">
+					<tr className="border-b-2 text-lg">
+						<th className="">No</th>
+						<th className="">Date</th>
+						<th className="">Mission</th>
+						<th className="">Rocket</th>
+						<th className="">Destination</th>
 					</tr>
 					{launchTable}
 				</table>

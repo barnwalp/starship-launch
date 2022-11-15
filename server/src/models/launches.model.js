1d1f4a8cd0f0dafa8abe94f83035d3993a6bd6ee
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-11-15 08:48:25 +0530

    feat/getting launch details as table in upcoming section

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,13 +1,25 @@
-const launch = {
-	flightNumber: 100,
-	mission: 'Kepler Exploration X',
-	rocket: 'Explorer IS1',
-	launchDate: new Date('December 21, 2028'),
-	destination: 'kepler-442 b',
-	customer: ['ISRO', 'NASA'],
-	upcoming: true,
-	success: true
-}
+const launch = [
+	{
+		flightNumber: 100,
+		mission: 'Kepler Exploration X',
+		rocket: 'Explorer IS1',
+		launchDate: new Date('December 21, 2028'),
+		destination: 'kepler-442 b',
+		customer: ['ISRO', 'NASA'],
+		upcoming: true,
+		success: true
+	},
+	{
+		flightNumber: 101,
+		mission: 'Kepler Exploration X',
+		rocket: 'Explorer IS2',
+		launchDate: new Date('January 28, 2029'),
+		destination: 'kepler-62 f',
+		customer: ['ISRO', 'NASA'],
+		upcoming: true,
+		success: true
+	},
+]
 
 function getLaunches() {
 	return launch;
--- view/src/Routes/Upcoming.jsx
+++ view/src/Routes/Upcoming.jsx
@@ -1,18 +1,43 @@
+import { useState, useEffect } from 'react';
 import { httpGetLaunches } from '../hooks/request';
 
 const Upcoming = () => {
-	const getLaunches = async () => {
-		let launches = await httpGetLaunches();
-		console.log(launches);
-		return launches;
-	}
-	getLaunches();
+	const [launches, setLaunches] = useState([]);
+
+	useEffect(() => {
+		const getLaunches = async () => {
+			let response = await httpGetLaunches();
+			setLaunches(response);
+		}
+		getLaunches();
+	}, [])
+	console.log(launches);
+	const launchTable = launches.map((launch) => {
+		return (
+			<tr>
+				<td>{launch.flightNumber}</td>
+				<td>{launch.launchDate}</td>
+				<td>{launch.mission}</td>
+				<td>{launch.rocket}</td>
+				<td>{launch.destinanation}</td>
+			</tr>
+		)
+	})
 	return (
 		<div className="h-screen">
 			<div className="w-2/3 p-6 border-2 mt-6 mx-auto">
 				<p className="text-2xl">Upcoming missions including both SpaceX launches and newly scheduled ISRO GSLV-III rockets.</p>
 				<p className="text-2xl mt-3">Warning! Clicking on the X aborts the mission.</p>
-				{/*}Table Header - No Date Mission Rocket Destination{*/}
+				<table>
+					<tr>
+						<th>No</th>
+						<th>Date</th>
+						<th>Mission</th>
+						<th>Rocket</th>
+						<th>Destination</th>
+					</tr>
+					{launchTable}
+				</table>
 			</div>
 		</div>
 	)

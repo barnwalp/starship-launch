88b722ff7ae6960909f9e586065ee60df6785794
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-11-16 22:07:55 +0530

    feat/created function to post request to /launches server

--- view/src/hooks/request.js
+++ view/src/hooks/request.js
@@ -14,7 +14,20 @@ async function httpGetLaunches() {
 	return launches;	
 }
 
+async function httpSubmitLaunch(launch) {
+	const response = await fetch('http://localhost:7000/launches', { 
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(launch),
+	});
+	const content = await response.json();
+	console.log(content);
+}
+
 export {
 	httpGetPlanets,
 	httpGetLaunches,
+	httpSubmitLaunch,
 };

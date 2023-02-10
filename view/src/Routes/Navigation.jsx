37ad75dab25c2b1a727c51ac8f0382c7edab9339
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-02-10 09:59:27 +0530

    chore/changed logo

--- view/src/Routes/Navigation.jsx
+++ view/src/Routes/Navigation.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as Logo } from '../img/solar2.svg'
 const Navigation = () => {
 	return (
 		<Fragment>
-			<nav className="flex py-4  border-transparent border-b-primary border-b-4 text-2xl space-x-10 justify-center">
+			<nav className="flex py-4 border-transparent border-b-primary border-b-4 text-2xl space-x-10 justify-center">
 				<h3>
 					<Logo className="w-8 inline-block animate-delay-spin duration-1000 mr-2"/> 
 					Starship Launch
